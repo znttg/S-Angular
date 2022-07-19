516ad4ab4d31fd4713b4dab63419cde4abdbaaa7
znttg <zanattagiovanni@gmail.com>
2022-07-19 14:09:02 -0300

    Uploading local files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FirstComponent} from "./first-component/first-component.component";
+import { SecondComponent } from './second-component/second-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/first-component/first-component.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'first-component',
+  template: `
+    <p>First angular component</p>
+  `
+})
+export class FirstComponent {
+
+}
