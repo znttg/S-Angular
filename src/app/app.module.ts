2ccb20b5551e84a8658b31ae781aa073128d0155
Giovanni Pires Zanatta <giovanni@centrus.org.br>
2022-07-19 16:19:18 -0300

    Creation of modules and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FirstComponent} from "./first-component/first-component.component";
 import { SecondComponent } from './second-component/second-component.component';
+import { CursosModule } from "./cursos/cursos.module";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SecondComponent } from './second-component/second-component.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
